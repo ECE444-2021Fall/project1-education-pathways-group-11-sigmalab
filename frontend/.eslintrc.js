93d7568ea17007f158be36ae479c6e3ac98cfb49
Adel Aswad <aswadadel@gmail.com>
2021-10-20 23:06:30 -0400

    configure eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -8,9 +8,10 @@ module.exports = {
       jsx: true,
     },
   },
-  plugins: ['@typescript-eslint', 'react-hooks'],
+  plugins: ['react', '@typescript-eslint', 'react-hooks'],
   extends: [
     'eslint:recommended',
+    'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
@@ -18,6 +19,7 @@ module.exports = {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 'off',
+    'no-console': [1, { allow: ['warn', 'error'] }],
   },
   settings: {
     react: {
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    // fontFamily: {
+    //   // lobster: "lobster",
+    //   // kalam: "kalam",
+    // },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
