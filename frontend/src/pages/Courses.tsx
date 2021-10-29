fd457b3a26380262b8b6e1c0edd33351e4af1764
Alexjsenn <alexsenn@Alexs-MacBook-Pro.local>
2021-10-29 00:01:02 -0400

    Add 2 column layout with cards, still needs visual improvemnt

--- frontend/src/pages/Courses.tsx
+++ frontend/src/pages/Courses.tsx
@@ -4,10 +4,26 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import { Typography } from '@mui/material';
 
 function Courses(): JSX.Element {
+  const wideCol = [
+    card(
+      'Description',
+      'The software development process. Software requirements and specifications. Software design techniques. Techniques for developing large software systems; CASE tools and software development environments. Software testing, documentation and maintenance'
+    ),
+    card('Offered', 'Fall 2021 Winter 2022'),
+  ];
+  const narrowCol = [
+    card('Division', 'Faculty of Applied Science & Engineering'),
+    card(
+      'Department',
+      'Edward S. Rogers Sr. Dept. of Electrical & Computer Engin.'
+    ),
+  ];
   return (
-    <Box sx={{ flexGrow: 1 }}>
+    <Box height="100vh" bgcolor="primary.main">
       <Grid
         container
         direction='row'
@@ -17,41 +33,41 @@ function Courses(): JSX.Element {
         columnSpacing={0}>
         <Grid item xs={12}>
           <div tw='flex justify-center items-center h-full w-full'>
-            <h1
-              css={[
-                tw`text-5xl text-black padding-bottom[2rem] padding-top[5rem]`,
-              ]}>
+            <Typography gutterBottom variant="h2" mt={4} color="white">
               ECE444: Software Engineering
-            </h1>
+            </Typography>
           </div>
         </Grid>
         <Grid item xs={8}>
-          {card(
-            'Description',
-            'The software development process. Software requirements and specifications. Software design techniques. Techniques for developing large software systems; CASE tools and software development environments. Software testing, documentation and maintenance'
-          )}
+          {customColumn(wideCol)}
         </Grid>
         <Grid item xs={4}>
-          {card('Division', 'Faculty of Applied Science & Engineering')}
+          {customColumn(narrowCol)}
         </Grid>
-      </Grid>
-      <Grid item xs={4}>
-        {card(
-          'Department',
-          'Edward S. Rogers Sr. Dept. of Electrical & Computer Engin.'
-        )}
       </Grid>
     </Box>
   );
 }
 
+function customColumn(children: JSX.Element[]): JSX.Element {
+  const gridItems = [];
+  for (const child of children) {
+    gridItems.push(<Grid item>{child}</Grid>);
+  }
+  return (
+    <Grid container direction='column' alignItems='stretch'>
+      {gridItems}
+    </Grid>
+  );
+}
+
 function card(title: string, content: string): JSX.Element {
   return (
-    <div tw='padding[2rem]'>
-      <Card sx={{ minWidth: 275 }}>
-        <CardContent tw='bg-blue-400 padding[4rem]'>
-          <h1 tw='text-2xl font-bold padding[5px]'> {title}</h1>
-          <h1 tw='text-2xl font-light'>{content}</h1>
+    <div tw='padding[1rem]'>
+      <Card sx={{ minWidth: 275 }} raised={true}>
+        <CardContent tw="bg-blue-200">
+          <Typography variant='h6' gutterBottom tw="padding-left[1rem]">{title}</Typography>
+          <Typography variant='body2' gutterBottom tw="padding-left[2rem]">{content}</Typography>
         </CardContent>
       </Card>
     </div>
