7950a534e948c8d01d2a0083bc2ea194eab35e22
Alexjsenn <alexsenn@Alexs-MacBook-Pro.local>
2021-10-31 23:26:56 -0400

    minor changes

--- frontend/src/pages/Courses.tsx
+++ frontend/src/pages/Courses.tsx
@@ -1,39 +1,34 @@
 import React from 'react';
 import tw from 'twin.macro';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import { Typography } from '@mui/material';
+import { Box, Card, CardContent, Grid, Typography } from '@mui/material';
 
 function Courses(): JSX.Element {
   const wideCol = [
-    card(
-      'Description',
-      'The software development process. Software requirements and specifications. Software design techniques. Techniques for developing large software systems; CASE tools and software development environments. Software testing, documentation and maintenance'
-    ),
-    card('Offered', 'Fall 2021 Winter 2022'),
+    card('Description', [
+      'The software development process. Software requirements and specifications. Software design techniques. Techniques for developing large software systems; CASE tools and software development environments. Software testing, documentation and maintenance',
+    ]),
+    card('Offered', ['Fall 2021', 'Winter 2022', 'Summer 2022']),
   ];
   const narrowCol = [
-    card('Division', 'Faculty of Applied Science & Engineering'),
-    card(
-      'Department',
-      'Edward S. Rogers Sr. Dept. of Electrical & Computer Engin.'
-    ),
+    card('Division', ['Faculty of Applied Science & Engineering']),
+    card('Department', [
+      'Edward S. Rogers Sr. Dept. of Electrical & Computer Engin.',
+    ]),
+    card('Prerequisites', ['ECE345', 'ECE297']),
   ];
   return (
-    <Box height="100vh" bgcolor="primary.main">
+    <Box tw='h-screen bg-gray-light'>
       <Grid
         container
         direction='row'
         justifyContent='space-around'
         alignItems='flex-start'
         rowSpacing={0}
-        columnSpacing={0}>
+        columnSpacing={0}
+      >
         <Grid item xs={12}>
           <div tw='flex justify-center items-center h-full w-full'>
-            <Typography gutterBottom variant="h2" mt={4} color="white">
+            <Typography gutterBottom variant='h2' mt={4} color='black'>
               ECE444: Software Engineering
             </Typography>
           </div>
@@ -61,13 +56,23 @@ function customColumn(children: JSX.Element[]): JSX.Element {
   );
 }
 
-function card(title: string, content: string): JSX.Element {
+function card(title: string, content: string[]): JSX.Element {
+  const cardContent = [];
+  for (const text of content) {
+    cardContent.push(
+      <Typography variant='body1' tw='padding-left[1rem]'>
+        {text}
+      </Typography>
+    );
+  }
   return (
     <div tw='padding[1rem]'>
       <Card sx={{ minWidth: 275 }} raised={true}>
-        <CardContent tw="bg-blue-200">
-          <Typography variant='h6' gutterBottom tw="padding-left[1rem]">{title}</Typography>
-          <Typography variant='body2' gutterBottom tw="padding-left[2rem]">{content}</Typography>
+        <CardContent tw='bg-blue-300'>
+          <Typography variant='h6' gutterBottom>
+            {title}
+          </Typography>
+          {cardContent}
         </CardContent>
       </Card>
     </div>
