abe6cfad82c3f5eef50eba2f3c69265360f7abc5
Adel Aswad <aswadadel@gmail.com>
2021-11-08 10:52:07 -0500

    edit order of initialization script

--- education_pathways/__init__.py
+++ education_pathways/__init__.py
@@ -3,9 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# NOTE: Uncomment this to re-populate db
-# from . import db_init
-
 # ------------------------------------>  db system    username:passwd   host     db-name
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@database/postgres'
 
@@ -14,7 +11,10 @@ db = SQLAlchemy(app)
 from . import routes
 from . import models
 
+# NOTE: Uncomment this to re-populate db
 # db.drop_all()
+# from . import db_init
+
 db.create_all()
 
 if __name__=="__main__":
