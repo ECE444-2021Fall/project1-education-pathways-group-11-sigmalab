c96bce869d8c9fd1d89613af7803e5d034257303
Adel Aswad <aswadadel@gmail.com>
2021-11-11 02:28:04 -0500

    add simple user_profile endpoints

--- education_pathways/routes/profile_routes.py
+++ education_pathways/routes/profile_routes.py
@@ -1,20 +1,53 @@
 from . import app, db
 from flask import jsonify, request
-from ..models.profiles import Profile
+from ..models.profiles import Course_Profile_A, Profile
 from ..models.users import User
 from ..models.courses import Course
 
 # simple profile.create endpoint using cascades
 @app.route('/addProfile', methods=['POST'])
 def addProfile():
-  user = User.query.limit(1).one()
-  course = Course.query.filter_by(id = 120).one()
+  print('------------begin----------', flush=True)
+  data = request.json
 
-  newProfile = Profile(creator=user, courses=[course])
-  db.session.add(newProfile)
+  # get the profile
+  profile = db.session.query(Profile).join(Profile, User.profiles).\
+    filter(User.username==data['username']).\
+    filter(Profile.name==data['name']).one()
+
+  # get the courses
+  course_ids = []
+  course_sessions = []
+
+  for course in data['courses']:
+    course_ids.append(course['id'])
+    course_sessions.append(course['session'])
+
+  courses_query = Course.query.filter(Course.id.in_(course_ids)).all()
+  courses = [(course,course_sessions[index]) \
+    for index, course in enumerate(courses_query)]
+
+  # newProfile = Profile(name=data['name'], creator=user)
+  profile.add_courses(courses)
+  print(profile, flush=True)
   try:
     db.session.commit()
   except Exception as e:
     print(e, flush=True)
     return jsonify(success=False), 400
+  db.session.add(profile)
+  print('-------------end-----------', flush=True)
+  return jsonify(success=True), 200
+
+@app.route('/getProfile', methods=['GET'])
+def getProfile():
+  print('------------begin----------', flush=True)
+  data = request.json
+  # user = User.query.filter_by(username=data['username']).one()
+  # profile = user.profiles[0]
+  profile = db.session.query(Profile).join(Profile, User.profiles).\
+    filter(User.username==data['username']).\
+    filter(Profile.name==data['name']).one()
+  print(profile.courses, flush=True)
+  print('-------------end-----------', flush=True)
   return jsonify(success=True), 200
--- education_pathways/routes/user_routes.py
+++ education_pathways/routes/user_routes.py
@@ -1,14 +1,16 @@
-from flask.wrappers import Request
 from . import app, db
 from flask import jsonify, request
 from ..models.users import User
+from ..models.profiles import Profile
 
 # EXAMPLE: import the model used to create new rows in a table
 @app.route('/addUser', methods=['POST'])
 def addUser():
   data = request.json
-  newUser = User(username=data['username'])
-  db.session.add(newUser)
+  new_user = User(username=data['username'])
+  new_user.profiles.append(Profile(name='wishlist'))
+
+  db.session.add(new_user)
   try:
     db.session.commit()
   except:
