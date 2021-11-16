3f84ed1d62dba696adf9272b49de52d2370d5d4f
Jay Bihola <39041724+jaybihola@users.noreply.github.com>
2021-11-15 22:22:17 -0500

    Allowed blog post props and filled with dummy info

--- frontend/src/components/Home/Blogpost.tsx
+++ frontend/src/components/Home/Blogpost.tsx
@@ -3,11 +3,17 @@ import tw from 'twin.macro';
 import { Card } from '../shared';
 import { Button } from '../shared';
 
-function Blogpost(): JSX.Element {
+interface BlogpostProps {
+  title: string;
+  body: string;
+  link: string;
+}
+
+function Blogpost({ title, body, link }: BlogpostProps): JSX.Element {
   return (
     <Card tw='m-2'>
-      <h1 tw='text-gray-800 text-2xl w-full'>Sample Post</h1>
-      <p tw='text-gray-600 text-lg'>This is a test post.</p> <br />
+      <h1 tw='text-gray-800 text-2xl w-full'>{title}</h1>
+      <p tw='text-gray-600 text-lg'>{body}</p> <br />
       <Button variant='primary' tw='justify-center'>
         Read More
       </Button>
--- frontend/src/components/Home/Whatsnew.tsx
+++ frontend/src/components/Home/Whatsnew.tsx
@@ -8,9 +8,18 @@ function Whatsnew(): JSX.Element {
     <Fragment>
       <Card tw='h-full w-1/2 flex flex-col justify-center mb-1'>
         <h1 tw='flex-auto text-3xl text-blue-uoft'>Whats New</h1>
-        <Blogpost tw='flex-auto'></Blogpost>
-        <Blogpost tw='flex-auto'></Blogpost>
-        <Blogpost tw='flex-auto'></Blogpost>
+        <Blogpost
+          title='Upcoming Features'
+          body='Sigma Educate will have a bunch of new features coming up. Please stay tuned as we work towards making the ultimate course selection app.'
+          link=''
+          tw='flex-auto'
+        />
+        <Blogpost
+          title='Just Launched'
+          body='Sigma Educate has just launched the MVP. Please provide feedback if you encounter any issues.'
+          link=''
+          tw='flex-auto'
+        />
       </Card>
     </Fragment>
   );
